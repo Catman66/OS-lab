e9f854d7616ec95cf8326838b2ef2f9b8e450e80
tracer-nju <tracer@nju.edu.cn>
2023-03-08 09:16:42 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:16:41 up 56 min,  0 users,  load average: 0.00, 0.00, 0.00

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <assert.h>
 
+
+
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]);
+
+  /*read the file of numeric*/
+  char *path_to_proc = "/proc/1/stat";
+  FILE* proc_reading = fopen(path_to_proc, "r");
+  assert(proc_reading != NULL);
+
+  /*read the info*/
+  char c[10];
+  int i = 0;
+  for(; i < 10-1; i++)
+  {
+    c[i] = fgetc(proc_reading);
+    if(c[i] == ' ')
+      break;
+  }
+  c[i] = '\0';
+  printf("%s\n", c);
+
   return 0;
 }
