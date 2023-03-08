6b1f9e7cf61ca479831d79c2bda5d741c13752b3
tracer-nju <tracer@nju.edu.cn>
2023-03-08 09:40:33 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:40:33 up  1:19,  0 users,  load average: 0.08, 0.02, 0.01

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <assert.h>
+#include <dirent.h>
 
+#ifndef TASK_COMM_LEN
+#define TASK_COMM_LEN 16
+#endif
 
+typedef struct 
+{
+  char name[TASK_COMM_LEN];
+  int pid;
+  int ppid;
+} Proc_info;
 
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]);
-
-  /*read the file of numeric*/
-  char *path_to_proc = "/proc/1/stat";
-  FILE* proc_reading = fopen(path_to_proc, "r");
-  assert(proc_reading != NULL);
-
-  /*read the info*/
+void read_proc_info(FILE* proc_read_begin, Proc_info* buff)
+{
   char c[10];
   int i = 0;
   for(; i < 10-1; i++)
   {
-    c[i] = fgetc(proc_reading);
+    c[i] = fgetc(proc_read_begin);
     if(c[i] == ' ')
       break;
   }
@@ -30,19 +29,43 @@ int main(int argc, char *argv[]) {
   int cnt_word = 1;
   while(cnt_word < 3)
   {
-    if(fgetc(proc_reading) == ' ')
+    if(fgetc(proc_read_begin) == ' ')
       cnt_word++;
   }
-
   
   for(i = 0; i < 10-1; i++)
   {
-    c[i] = fgetc(proc_reading);
+    c[i] = fgetc(proc_read_begin);
     if(c[i] == ' ')
       break;
   }
   c[i] = '\0';
   printf("%s\n", c);
 
+}
+
+int main(int argc, char *argv[]) {
+  for (int i = 0; i < argc; i++) {
+    assert(argv[i]);
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
+  assert(!argv[argc]);
+
+  /*read the file of numeric*/
+  DIR* proc_dir = opendir("/proc");
+  struct dirent* file_in_dir;
+  while((file_in_dir = readdir(proc_dir)) != NULL)
+  {
+    if(is_numeric_str(file_in_dir->d_name))
+    {
+      char path[] = "/proc";
+      strcat(path, file_in_dir->d_name);
+      printf("%s\n", path);
+    }
+  }
+
+  /*read the info*/
+  
+  
   return 0;
 }
