ca003983cedf41c2a470be4861590ff05519b834
tracer-nju <tracer@nju.edu.cn>
2023-03-11 14:36:09 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:36:09 up  4:19,  0 users,  load average: 0.00, 0.00, 0.00

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -54,9 +54,10 @@ void splash() {
 
 // Operating system is a C program!
 int main(const char *args) {
+
   ioe_init();
 
-  puts("mainargs = \"");
+  puts("main args = \"");
   puts(args);  // make run mainargs=xxx
   puts("\"\n");
 
