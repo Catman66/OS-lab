2c53e408f389870c658d44e525479c54fba4580d
tracer-nju <tracer@nju.edu.cn>
2023-03-23 11:11:21 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:11:21 up  1:57,  0 users,  load average: 0.18, 0.16, 0.14

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -89,7 +89,7 @@ struct coordinate
 };
 
 #define IS_VALID_IJ(i, j) ((i>=0) && (i<M) && (j>=0) && (j<N))
-void first_ij(int round, int tid, struct coordinate* buff)
+void first_pos(int round, int tid, struct coordinate* buff)
 {
   int workload_of_round = workload(round),
   average = workload_of_round/T;
@@ -104,7 +104,7 @@ void calculate(int tid)
   int round = progresses[tid]+1;
 
   struct coordinate position;
-  first_ij(round, tid, &position);
+  first_pos(round, tid, &position);
 
   for(int t = 0; t < workload_thread(round, tid); t++, RENEW_POSISTION(position))
   {
@@ -120,28 +120,76 @@ void calculate(int tid)
   }
 }
 
-void display_dp_mtx()
+#define limit_need_concurrent 100
+#define CONCURRENT_CALCULATE(id) before_calculating(id);\
+    calculate(id);\
+    after_calculating(id)
+    
+
+void single_worker_finish_round(int round){
+  struct coordinate position;
+  first_pos(round, T, &position);
+
+  for(int step = 0; step < workload(round); step++, RENEW_POSISTION(position)){
+    int i = position.i;
+    int j = position.j;
+
+    assert(IS_VALID_IJ(i, j));
+    int skip_a = DP(i-1, j);
+    int skip_b = DP(i, j-1);
+    int take_both = DP(i-1, j-1) + (A[i] == B[j]);
+
+    dp[i][j] = MAX3(skip_a, skip_b, take_both);
+  }
+
+  for(int i = 1; i <= T; i++)
+    progresses[i]++;
+}
+
+void Tworker(int id) {
+  for (int round = 0; round < M + N - 1; round++) {
+    if(workload(round) < limit_need_concurrent){
+      continue;
+    }
+
+    CONCURRENT_CALCULATE(id);
+  }
+}
+
+
+void Tsuper_worker()
 {
-  for(int i = 0 ; i < M; i++)
-  {
+  //id == T;
+  for (int round = 0; round < M + N - 1; round++) {
+    if(workload(round) < limit_need_concurrent){
+      single_worker_finish_round(round);
+      continue;
+    }
+    CONCURRENT_CALCULATE(T);
+  }
+}
+
+//
+
+void single_worker_finish_all(){
+  for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        // Always try to make DP code more readable
+        int skip_a = DP(i - 1, j);
+        int skip_b = DP(i, j - 1);
+        int take_both = DP(i - 1, j - 1) + (A[i] == B[j]);
+        dp[i][j] = MAX3(skip_a, skip_b, take_both);
+      }
+    }
+}
+
+void display_dp_mtx(){
+  for(int i = 0 ; i < M; i++){
     for(int j = 0; j < N; j++)
       printf("%d ", dp[i][j]);
     printf("\n");
   }
 }
-
-void Tworker(int id) {
-  for (int round = 0; round < M + N - 1; round++) {
-    before_calculating(id);
-
-    calculate(id);
-
-    after_calculating(id);
-  }
-  
-}
-
-
 
 int main(int argc, char *argv[]) {
   // No need to change
@@ -155,28 +203,20 @@ int main(int argc, char *argv[]) {
   for(int t = 1; t <= T; t++)
     assert(progresses[t] == -1);
   
-  if(T == 1)
-  {
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < M; j++) {
-        // Always try to make DP code more readable
-        int skip_a = DP(i - 1, j);
-        int skip_b = DP(i, j - 1);
-        int take_both = DP(i - 1, j - 1) + (A[i] == B[j]);
-        dp[i][j] = MAX3(skip_a, skip_b, take_both);
-      }
-    }
+  if(T == 1){
+    single_worker_finish_all();
   }
   else{
     //thread id: 1, 2, 3, ..., T
-    for (int i = 0; i < T; i++) {
+    for (int i = 0; i < T-1; i++) {
       create(Tworker);
     }
+    create(Tsuper_worker);//在工作量未达到一定量之前，不并行，而是由super worker完成工作
+
     join();  // Wait for all workers
   }
   result = dp[M - 1][N - 1];
   printf("%d\n", result);
-  //display_dp_mtx();
   FREE_PROGRESSES();
 
   return 0;
