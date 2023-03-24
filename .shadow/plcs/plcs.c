6f6316f1bfd1522f0c71c281f910226b840d110a
tracer-nju <tracer@nju.edu.cn>
2023-03-24 10:53:09 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:53:09 up  3:13,  0 users,  load average: 0.36, 0.14, 0.04

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -113,7 +113,7 @@ void calculate(int tid, int round) {
   //printf(" %d id finished %d round\n", tid, round);
 }
 
-#define limit_need_concurrent 200
+#define limit_need_concurrent 2000
 #define CONCURRENT_CALCULATE(tid, round) before_calculating(round);\
     calculate(tid, round);\
     after_calculating(round)
