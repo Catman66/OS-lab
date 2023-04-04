cdf73354fdc9c4cd153f855db0b840bc63e592e5
tracer-nju <tracer@nju.edu.cn>
2023-04-04 20:43:48 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:43:48 up  2:45,  0 users,  load average: 0.14, 0.11, 0.04

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -9,9 +9,7 @@ static void kfree(void *ptr) {
 
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
-  putstr("hello before\n");
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
-  putstr("hello 2 \n");
 }
 
 MODULE_DEF(pmm) = {
