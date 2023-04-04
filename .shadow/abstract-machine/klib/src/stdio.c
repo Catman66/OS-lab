d77e0e01ba639ab39776ff53ae1ebf6d8b47de63
tracer-nju <tracer@nju.edu.cn>
2023-04-04 08:45:05 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:45:05 up 33 min,  0 users,  load average: 0.24, 0.11, 0.03

--- .shadow/abstract-machine/klib/src/stdio.c
+++ .shadow/abstract-machine/klib/src/stdio.c
@@ -6,8 +6,16 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+#define PRINTF_BUFFER_SIZE 256
 int printf(const char *fmt, ...) {
-  panic("printf Not implemented");
+  va_list p_lst;
+  va_start(p_lst, fmt);
+  
+  char buff[PRINTF_BUFFER_SIZE];
+  int ret = vsnprintf(buff, PRINTF_BUFFER_SIZE, fmt, p_lst);
+
+  va_end(p_lst);
+  return ret;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -23,7 +31,21 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  //details here 
+  size_t i_fmt, i_out;
+  for(i_fmt = 0, i_out = 0; i_fmt < n && fmt[i_fmt]; i_fmt++){
+    switch(fmt[i_fmt]){
+      case '%':
+        //read the length, var type
+        //conversion the value, to string
+        //append, update i_fmt & i_out
+        
+      default :
+        out[i_out++] = fmt[i_fmt];
+    }
+  }
+
+  return i_out;
 }
 
 #endif
