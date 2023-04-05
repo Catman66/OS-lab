de5be93d67b5639fa055c83e6c8c7aa7863c2ddd
tracer-nju <tracer@nju.edu.cn>
2023-04-05 09:30:35 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:30:35 up 54 min,  0 users,  load average: 0.08, 0.06, 0.02

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -1,14 +1,73 @@
 #include <common.h>
 
+struct Heap_node{
+  uintptr_t size;
+  struct Heap_node* next;
+} HEAP_HEAD;
+typedef struct Heap_node Heap_node;
+
+void init_heap_node(Heap_node* nd, uintptr_t sz, Heap_node* nxt){
+  nd->size = sz;
+  nd->next = nxt;
+}
+
+#define HEAP_HEAD_SIZE (sizeof(void*) + sizeof(Heap_node*))
+#define INIT_HEAP_HEAD(heap_sz) \
+HEAP_HEAD.next = heap.start; init_heap_node(HEAP_HEAD.next, heap_sz - HEAP_HEAD_SIZE, NULL)
+
+void* end_of_node(Heap_node* nd){
+  return (void*)nd + HEAP_HEAD_SIZE + nd->size;
+}
+
 static void *kalloc(size_t size) {
-  return NULL;
+  size_t required_sz = size + HEAP_HEAD_SIZE;
+  Heap_node* p, *pre;
+  for(pre = &HEAP_HEAD, p = HEAP_HEAD.next; p != NULL; pre = p, p=p->next){
+    if(p->size < required_sz){
+      continue;
+    }
+    if(p->size == required_sz){/*just remove the node from list*/
+      pre->next = p->next;
+      break;
+    }
+    /*reduce the size of the node*/
+    p->size -= required_sz;
+    p = end_of_node(p);
+  }
+  if(p == NULL){
+    return NULL;
+  }
+  return (void*)p + HEAP_HEAD_SIZE;
+}
+
+void merge_node(Heap_node* reslt, Heap_node* merged){
+  reslt->size += merged->size + HEAP_HEAD_SIZE;
 }
 
 static void kfree(void *ptr) {
+  /*find the position*/
+  uintptr_t freed = (uintptr_t)ptr - HEAP_HEAD_SIZE;
+
+  Heap_node* p, *pre;
+  for(pre = &HEAP_HEAD, p = HEAP_HEAD.next; p != NULL; pre = p, p = p->next){
+    if(freed < (uintptr_t)p){
+      break;
+    }
+  }
+  if(end_of_node((Heap_node*)freed) == p){
+    merge_node((Heap_node*)freed, p);
+  }
+  if((uintptr_t)end_of_node(pre) == freed){
+    merge_node(pre, (Heap_node*)freed);
+  }
 }
+
 
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
+  assert(pmsize >= HEAP_HEAD_SIZE);
+  INIT_HEAP_HEAD(pmsize);
+  
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 }
 
