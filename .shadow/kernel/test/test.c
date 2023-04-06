f9916c5d9568c3a1a0756b3e8da6176b534e9c94
tracer-nju <tracer@nju.edu.cn>
2023-04-06 11:55:34 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:55:34 up  3:40,  0 users,  load average: 0.28, 0.13, 0.05

--- .shadow/kernel/test/test.c
+++ .shadow/kernel/test/test.c
@@ -18,17 +18,20 @@ void print_cases(){
     }
 }
 
+
 void sort_cases(){
     for(int i = 0; i < SCALE - 1; i++){
         for(int j = 0; j < SCALE - 1 - i; j++){
             if(cases[j].sz > cases[j+1].sz){
-                
-                struct alloc_tst tmpt = cases[j];
-                cases[j] = cases[j+1];
-                cases[j+1] = tmpt;
+                printf("before swap: \n");
+                printf("c1: %p, c2: %p \n", cases[j].ptr, cases[j+1].ptr);
+                printf("after swap\n");
+                printf("c1: %p, c2: %p \n", cases[j].ptr, cases[j+1].ptr);
             }
         }
     }
+
+    assert(0);
     /*
     for(int i = 0; i < SCALE - 1; i++){
         if(!(cases[i].ptr < cases[i+1].ptr)){
