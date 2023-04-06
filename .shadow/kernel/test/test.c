07c19ac8fdd2fd6500d4a54385cd665eac60ad35
tracer-nju <tracer@nju.edu.cn>
2023-04-06 13:35:54 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:35:54 up 7 min,  0 users,  load average: 0.04, 0.05, 0.01

--- .shadow/kernel/test/test.c
+++ .shadow/kernel/test/test.c
@@ -38,15 +38,19 @@ void sort_cases(){
     for(int i = 0; i < SCALE - 1; i++){
         for(int j = 0; j < SCALE - 1; j++){
             if(cases[j].ptr > cases[j+1].ptr){
-                printf("before swap: \n");
-                printf("c1: %p, c2: %p \n", cases[j].ptr, cases[j+1].ptr);
+                //printf("before swap: \n");
+                //printf("c1: %p, c2: %p \n", cases[j].ptr, cases[j+1].ptr);
                 alloc_tst tmpt = cases[j];
                 cases[j] = cases[j+1];
                 cases[j+1] = tmpt;
-                printf("after swap\n");
-                printf("c1: %p, c2: %p \n", cases[j].ptr, cases[j+1].ptr);
+                //printf("after swap\n");
+                //printf("c1: %p, c2: %p \n", cases[j].ptr, cases[j+1].ptr);
             }
         }
+        for(int i = 0; i < SCALE; i++){
+            printf("%p  ", cases[i].ptr);
+        }
+        printf("\n");
     }
 
     
@@ -136,7 +140,7 @@ void check(){
 
 int main(){
     pmm->init();
-    //check();
-    check_sort();
+    check();
+    //check_sort();
     return 0;
 }
