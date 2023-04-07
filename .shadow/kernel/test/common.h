3a577cb4dacee60e5d73c94409fa8d458533c589
tracer-nju <tracer@nju.edu.cn>
2023-04-07 14:23:02 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:23:02 up  2:37,  0 users,  load average: 0.50, 0.18, 0.06

--- .shadow/kernel/test/common.h
+++ .shadow/kernel/test/common.h
@@ -2,7 +2,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include<assert.h>
 
 #define ROUNDUP(a, sz)      ((((uintptr_t)a) + (sz) - 1) & ~((sz) - 1))
 #define ROUNDDOWN(a, sz)    ((((uintptr_t)a)) & ~((sz) - 1))
