935393a08cfbdd1952b6b7b7be304b2d886d1f4e
tracer-nju <tracer@nju.edu.cn>
2023-04-07 18:44:44 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:44:44 up  6:59,  0 users,  load average: 0.00, 0.05, 0.05

--- .shadow/kernel/test/threads-test.c
+++ .shadow/kernel/test/threads-test.c
@@ -5,14 +5,14 @@ void Tclient(int tid){
     STK stk;
     printf("hello from thread %d \n", tid);
     while(1){
-        switch (rand_act())
-        {
-        case ACT_ALLOC:
-            do_alloc(&stk);
-            break;
-        case ACT_FREE:
-            do_free(&stk);
-            break;
+        printf("hello \n");
+        switch (rand_act()){
+            case ACT_ALLOC:
+                do_alloc(&stk);
+                break;
+            case ACT_FREE:
+                do_free(&stk);
+                break;
         }
     }
 }
