7c6c360071f87c2ed57ed3e1fa37f6e4eac2b86d
tracer-nju <tracer@nju.edu.cn>
2023-04-07 17:16:34 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:16:34 up  5:31,  0 users,  load average: 0.02, 0.03, 0.02

--- .shadow/abstract-machine/klib/src/string.c
+++ .shadow/abstract-machine/klib/src/string.c
@@ -42,7 +42,11 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  char *p = s;
+  while(n > 0){
+    *(p++) = c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
