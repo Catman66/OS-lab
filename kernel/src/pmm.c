f0e81d9357511a41097498a2c7970b689b842c65
catman <xlglmxx@163.com>
2023-04-08 10:57:38 +0800

    0408 morning 02

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -57,7 +57,7 @@ void check_paint(Heap_node* nd, char val){
   for(char* p = (char*)FREE_SPACE_BEGIN(nd); INTP(p) < FREE_SPACE_END(nd); p++){
     if(*p != val){
       printf("===CHECK_PAINT ERROR node: %p,  expected: %x, actual: %x === \n", nd, (uint8_t)val, (uint8_t)*p);
-      print_context(p, p + HEAP_HEAD_SIZE);
+      //print_context(p, p + HEAP_HEAD_SIZE);
       assert(0);
     }
   }
