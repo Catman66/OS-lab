33148feb69ce6ed27d1e90fedd261d5d8ad10108
tracer-nju <tracer@nju.edu.cn>
2023-04-08 09:25:34 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:25:34 up  1:16,  0 users,  load average: 0.14, 0.11, 0.09

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -56,7 +56,10 @@ void check_paint(Heap_node* nd, char val){
   for(char* p = (char*)FREE_SPACE_BEGIN(nd); INTP(p) < FREE_SPACE_END(nd); p++){
     if(*p != val){
       printf("node: nd:%p,  expected: %x, actual: %x \n", nd, (uint8_t)val, (uint8_t)*p);
-      printf("contex: %p, %p\n", *(uintptr_t*)p, *(uintptr_t*)(p + sizeof(uintptr_t)));
+      uint8_t* tmpt = p;
+      for(int i = 0; i < HEAP_HEAD_SIZE; i++){
+        printf("%x ", *tmpt);
+      }
       assert(0);
     }
   }
