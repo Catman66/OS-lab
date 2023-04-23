02eac708d826f1be9efed9fb52cc9d5a7a90e1c4
catman66 <xlglmxx@163.com>
2023-04-23 21:31:33 +0800

    04-23-evening

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,7 +6,9 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-#define STR_BUFFER_SIZE 256
+#define STR_BUFFER_SIZE 1024
+#define CONVERSION_BUFFER_SIZE 64
+
 int printf(const char *fmt, ...) {
   va_list para_lst;
   va_start(para_lst, fmt);
@@ -15,10 +17,10 @@ int printf(const char *fmt, ...) {
   size_t len_fmt = strlen(fmt), len_read;
   
   while(len_read = vsnprintf_ret_len(formated, STR_BUFFER_SIZE, fmt, &para_lst) < len_fmt){
-    
+    fmt += len_read, len_fmt -= len_read;
   }
-  
-  return 0;
+  va_end(para_lst);
+  return len_fmt;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -38,7 +40,38 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 }
 
 int vsnprintf_ret_next_idx(char* out, size_t n, const char * fmt, va_list* ap){
+  char buffer[CONVERSION_BUFFER_SIZE];
+  char * dst = out;
+  
+  int len;
+  while(out - dst < n && *fmt){
+    if(*fmt != '%'){
+      *(out++) = *(fmt++);
+      continue;
+    }
+    fmt++;
+    char * mov_from = NULL;
+    switch(*(fmt++)){
+      case 'd':
+        len = itoa(buffer, va_arg(*ap, int));
+        mov_from = buffer;
+        break;
+      case 'p':
+        len = htoa(buffer, va_arg(*ap, intptr_t));
+        mov_from = buffer;
+      case 's':
+        mov_from = va_arg(*ap, char*);
+        len = strlen(mov_from);
+      default:
+        putch(*fmt);
+        panic_on(0, " type not implemented\n");
+    }
+    panic_on(out - dst + len > n, "too long string in my vsnprintf\n");
+    out += len;
+    strcpy(out, mov_from);
+  }
 
+  
 }
 
 
