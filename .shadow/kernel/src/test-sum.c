8bb380ad2880bdffbc974e70083844774664c596
tracer-nju <tracer@nju.edu.cn>
2023-05-06 22:27:40 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:27:40 up  7:26,  0 users,  load average: 0.01, 0.07, 0.05

--- .shadow/kernel/src/test-sum.c
+++ .shadow/kernel/src/test-sum.c
@@ -4,8 +4,8 @@
 #define NThread 4
 static volatile int s = 0; 
 void Tsum(){
-    for(volatile int i = 0; i < 10000; i++){
-        //
+    for(int i = 0; i < 10000; i++){
+        __sync_synchronize();
         s++;
     }
     printf("current sum : %d\n", s);
