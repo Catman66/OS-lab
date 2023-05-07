7616c7984a49111fb3bd1aac2cf93cce5f2e0cd2
tracer-nju <tracer@nju.edu.cn>
2023-05-07 10:24:22 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:24:22 up  2:31,  0 users,  load average: 0.02, 0.04, 0.06

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -150,7 +150,7 @@ void kmt_teardown(task_t *task){
 #define HOLD 0
 #define NHOLD 1
 void kmt_spin_init(spinlock_t *lk, const char *name){
-    lk->val = HOLD;
+    lk->val = NHOLD;
 }
 void kmt_spin_lock(spinlock_t *lk){
     while(atomic_xchg(&(lk->val), NHOLD) == NHOLD){
