52def22ce4d1f87a2ca15837c42612b5f2b15ca6
tracer-nju <tracer@nju.edu.cn>
2023-05-07 08:21:32 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:21:32 up 28 min,  0 users,  load average: 0.17, 0.11, 0.03

--- .shadow/kernel/src/test-sum.c
+++ .shadow/kernel/src/test-sum.c
@@ -2,17 +2,19 @@
 #include<common.h>
 
 #define NThread 4
+#define ADDED 10000
 static volatile int s_nlk = 0, s_lk = 0; 
 extern spinlock_t usr_lk;
 
+
 void Tsum(){
-    for(int i = 0; i < 100000000; i++){
-        s_nlk++;
+    for(int i = 0; i < ADDED; i++){
+        s_nlk = s_nlk + 2;
     }
     printf("without final sum: %d\n", s_nlk);
-    for(int i = 0; i < 100000000; i++){
+    for(int i = 0; i < ADDED; i++){
         kmt->spin_lock(&usr_lk);
-        s_lk++;
+        s_lk = s_lk + 2;
         kmt->spin_unlock(&usr_lk);
     }
     printf("with lock, final sum: %d \n", s_lk);
