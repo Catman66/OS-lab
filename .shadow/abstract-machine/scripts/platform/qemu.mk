e5e205d4e90c84fe7f7ca878184323608d2a87a6
tracer-nju <tracer@nju.edu.cn>
2023-05-07 19:24:35 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:24:35 up 11:31,  0 users,  load average: 0.04, 0.03, 0.00

--- .shadow/abstract-machine/scripts/platform/qemu.mk
+++ .shadow/abstract-machine/scripts/platform/qemu.mk
@@ -3,9 +3,9 @@
 LDFLAGS    += -N -Ttext-segment=0x00100000
 QEMU_FLAGS += -serial mon:stdio \
               -machine accel=tcg \
-              -smp "$(smp)" \
+              -smp 4 \
               -drive format=raw,file=$(IMAGE)
-
+#"$(smp)"
 build-arg: image
 	@( echo -n $(mainargs); ) | dd if=/dev/stdin of=$(IMAGE) bs=512 count=2 seek=1 conv=notrunc status=none
 
