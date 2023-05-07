dc8cddb8b82a39e585450ac62abbf24922ed3a3a
tracer-nju <tracer@nju.edu.cn>
2023-05-07 09:09:31 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:09:31 up  1:16,  0 users,  load average: 0.02, 0.18, 0.14

--- .shadow/kernel/src/test-sum.c
+++ .shadow/kernel/src/test-sum.c
@@ -11,7 +11,6 @@ void Tsum(){
     printf("num cpu: %d\n", cpu_count());
     for(int i = 0; i < ADDED; i++){
         s_nlk++;
-        printf("hello\n");
     }
     printf("without final sum: %d\n", s_nlk);
     for(int i = 0; i < ADDED; i++){
