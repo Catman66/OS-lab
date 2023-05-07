41a9433042c25427297c61e6bad3a59f9c5025e0
tracer-nju <tracer@nju.edu.cn>
2023-05-07 10:06:51 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:06:51 up  2:13,  0 users,  load average: 0.27, 0.17, 0.06

--- .shadow/kernel/src/test-sum.c
+++ .shadow/kernel/src/test-sum.c
@@ -2,7 +2,7 @@
 #include<common.h>
 
 #define NThread 4
-#define ADDED 10000
+#define ADDED 100000
 static volatile int s_nlk = 0, s_lk = 0;
 static volatile int tool = 1; 
 extern spinlock_t usr_lk;
