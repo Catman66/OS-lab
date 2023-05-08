408a1d17f0ab620e6d7f7badd7e28f9fb37605ca
tracer-nju <tracer@nju.edu.cn>
2023-05-08 11:39:32 +0800

    Linux SuperLaptopOfCatman 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:39:32 up  3:32,  0 users,  load average: 0.19, 0.12, 0.10

--- .shadow/abstract-machine/klib/src/string.c
+++ .shadow/abstract-machine/klib/src/string.c
@@ -28,7 +28,14 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  for(; *s1 || *s2; s1++, s2++){
+    if(*s1 < *s2){
+      return -1;
+    } else if(*s1 > *s2){
+      return 1;
+    }
+  }
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
