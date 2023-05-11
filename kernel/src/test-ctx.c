76d32099e8eb49c3a3b3ef04d6e2f0064c0673b7
catman66 <xlglmxx@163.com>
2023-05-11 23:16:17 +0800

    night 0511

---
+++ kernel/src/test-ctx.c
@@ -0,0 +1,24 @@
+#include <common.h>
+#include <os.h>
+
+static void Ttest(void * arg){
+    uint8_t taken[176];
+    for(int i = 0 ; i < 176; i++){
+        taken[i] = i;
+    }
+    for(int i = 0 ; i < 176; i++){
+        printf("[%d]", taken[i]);
+        yield();
+    }
+    while(1);
+}
+
+const char * names[] = {
+    "T0","T1","T2","T3" 
+};
+
+void test_ctx(){
+    for(int i = 0; i < 1; i++){
+        kmt->create(tsk_alloc(), names[i], Ttest, NULL);
+    }
+}
